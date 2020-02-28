1a6a961760dd4009740a0021ac9d572471059994
Miranda Musselman <mirandamusselman@Pams-MacBook-Pro.local>
2020-02-28 11:09:40 -0700

    first commit

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+// export default function CharacterCard() {
+//   return <span>todo: character</span>;
+// }
+
+const CharacterCard = props => {
+  return (
+    <span>
+      <Cards key={props.id}>
+          <NamePlate>
+              <CharacterName>
+                  <NewH2>{props.name}</NewH2>
+              </CharacterName>
+          </NamePlate>
+          <DescPlate>
+              <Descriptions>
+                  <p>Status: {props.status}</p>
+                  <p>Species: {props.species}</p>
+                  <p>Gender: {props.gender}</p>
+              </Descriptions>
+          </DescPlate>
+      </Cards>
+    </span>
+  )
 }
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState } from "react";
+import Axios from 'axios';
+import CharacterCard from './CharacterCard'
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+function CharacterList() {
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+    Axios
+      .get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response.results);
+        setCharacters(response.results);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map((character, index) => {
+        return (
+          <CharacterCard
+            key={index}
+            name={character.name}
+            status={character.status}
+            species={character.species}
+            gender={character.gender}
+          />
+        );
+      })};
     </section>
   );
 }
+
+export default CharacterList
